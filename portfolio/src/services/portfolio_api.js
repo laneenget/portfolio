06275f0189d547083fc4e56e0a032171ac304f93
Lane Enget <lane.enget@go.minneapolis.edu>
2020-05-05 14:03:20 -0500

    add portfolio-api logic

--- portfolio/src/services/portfolio_api.js
+++ portfolio/src/services/portfolio_api.js
@@ -2,4 +2,27 @@ import axios from 'axios'
 
 export default {
     
+    getAllMessages() {
+        return axios.get('/api/messages').then(response => {
+            return response.data
+        })
+    },
+
+    getMessage(id) {
+        return axios.get('/api/messages/' + id).then(response => {
+            return response.data
+        })
+    },
+
+    addMessage(message) {
+        return axios.post('/api/messages', message).then(response => {
+            return response.data
+        })
+    },
+
+    deleteMessage(id) {
+        return axios.delete('/api/messages/' + id).then(response => {
+            return response.data
+        })
+    },
 }
