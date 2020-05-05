2c25480365b08d4b66d349d81b89bad1cdf604ca
Lane Enget <lane.enget@go.minneapolis.edu>
2020-05-05 14:30:00 -0500

    Update server.js

--- models/message.js
+++ models/message.js
@@ -3,14 +3,17 @@ module.exports = (sequelize, DataTypes) => {
     let Message = sequelize.define('Message', {
         name: {
             type: DataTypes.STRING,
+            allowNull: false
         }, email: {
             type: DataTypes.STRING,
+            allowNull: false
         }, message: {
             type: DataTypes.STRING,
+            allowNull: false
         }
     })
 
-    Message.syng({force: true}).then( () =>{
+    Message.syng({force: false}).then( () =>{
         console.log('synced message table')
     })
 
--- server.js
+++ server.js
@@ -11,6 +11,18 @@ app.use(bodyParser.json())
 
 app.use('/api', api_routes)
 
+//Handle routes not found
+app.use(function(req, res, next){
+    res.status(404).send('Not found')
+})
+
+//Handle server errors
+app.use(function (err, req, res, next){
+    console.error(err.stack)
+    res.status(500).send('Server error')
+})
+
+//Start server
 let server = app.listen(process.env.PORT || 3000, function() {
     console.log('Express server running on port', server.address().port)
 })
