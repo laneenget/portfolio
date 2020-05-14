45500c8dbe20c03239e2497c5725fe50fa85a955
Lane Enget <lane.enget@go.minneapolis.edu>
2020-05-14 15:17:09 -0500

    fix api.js

--- routes/api.js
+++ routes/api.js
@@ -10,11 +10,5 @@ let Mailgun = require('mailgun-js')
 let router = express.Router()
 let mailgun = Mailgun({apiKey: api_key, domain: domain})
 
-mailgun.post('/routes', {"priority": 0, "description":"Sample route",
-"expression": 'match_recipient(".*@sandboxcb6d364f0c354194ad7fd7956f40b11e.mailgun.org")',
-"action": 'forward("enget.lane@gmail.com")', "action": 'stop()'},
-function(error, body){
-    console.log(body);
-})
 
 module.exports = router
